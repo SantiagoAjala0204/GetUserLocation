69e71fe6596bc55c90e610511bff127294bf0f29
SantiagoAjala0204 <santiago.ajala@yachaytech.edu.ec>
2023-10-03 23:53:22 -0500

    new_bottons

--- app/src/main/java/com/example/getuserlocation/MainActivity.kt
+++ app/src/main/java/com/example/getuserlocation/MainActivity.kt
@@ -8,40 +8,101 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.io.IOException
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    private val locationService:LocationService= LocationService()
+    private val locationService: LocationService = LocationService()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tvLocation= findViewById<TextView>(R.id.tvLocation)
+        val tvLocation = findViewById<TextView>(R.id.tvLocation)
         val btnLocation = findViewById<Button>(R.id.btnLocation)
 
-        btnLocation.setOnClickListener{
+        btnLocation.setOnClickListener {
             lifecycleScope.launch {
                 val result = locationService.getUserLocation(this@MainActivity)
-                if(result!=null){
+                if (result != null) {
                     val latitude = result.latitude
                     val longitude = result.longitude
-                    tvLocation.text= "Latitud ${result.latitude} y longitud ${result.longitude}"
-                    // Luego, envía las coordenadas al servidor Flask
-                    sendCoordinatesToServer(result.latitude, result.longitude)
+                    tvLocation.text = "Latitud ${result.latitude} y longitud ${result.longitude}"
+
+                    // Obtener el ID del usuario ingresado por el usuario
+                    val etCedula = findViewById<EditText>(R.id.etCedula)
+                    val usuarioIdStr = etCedula.text.toString()
+                    // Obtener el nombre y apellido ingresados por el usuario
+                    val etNombre = findViewById<EditText>(R.id.etNombre)
+                    val etApellido = findViewById<EditText>(R.id.etApellido)
+                    val nombre = etNombre.text.toString()
+                    val apellido = etApellido.text.toString()
+
+                    // Intenta convertir el ID del usuario a un entero
+                    try {
+                        val usuarioId = usuarioIdStr.toInt()
+
+                        // Luego, envía las coordenadas y el ID del usuario al servidor Flask
+                        sendCoordinatesToServer(latitude, longitude, usuarioId)
+                        sendUserDataToServer(usuarioId, nombre, apellido)
+                    } catch (e: NumberFormatException) {
+                        // Maneja la excepción si el texto no es un número válido
+                        // Puedes mostrar un mensaje de error al usuario aquí
+                        println("Error: el ID del usuario no es un número válido")
+                    }
                 }
             }
         }
     }
-    private fun sendCoordinatesToServer(latitude: Double, longitude: Double) {
+
+    private fun sendCoordinatesToServer(latitude: Double, longitude: Double, userId: Int) {
         val client = OkHttpClient()
         val url = "http://192.168.0.21:5000/agregar_punto"  // Reemplaza con la URL de tu API
 
         val json = """
         {
             "latitud": "$latitude",
-            "longitud": "$longitude"
+            "longitud": "$longitude",
+            "usuario_id": $userId
+        }
+    """.trimIndent()
+
+        val requestBody = json.toRequestBody("application/json".toMediaType())
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) {
+                        // Manejar un error si la respuesta no es exitosa
+                        println("Error al enviar coordenadas al servidor")
+                    } else {
+                        // Procesar una respuesta exitosa si es necesario
+                        println("Coordenadas enviadas correctamente al servidor")
+                    }
+                }
+            }
+        })
+    }
+    private fun sendUserDataToServer(usuarioId: Int, nombre: String, apellido: String) {
+        val client = OkHttpClient()
+        val url = "http://192.168.0.21:5000/agregar_usuario"  // Reemplaza con la URL de tu API
+
+        val json = """
+        {
+            "id": $usuarioId,
+            "nombre": "$nombre",
+            "apellido": "$apellido"
+            
         }
     """.trimIndent()
 
